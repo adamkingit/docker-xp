4d70f142e72c81b87286253055418fbe3e0a1d83
Adam King <rak@us.ibm.com>
2016-08-07 15:02:50 -0400

    Build Docker container with UCD WebSphere plugins
    Initial drop of files to build a container with the WebSphere plugins.
    Include .travis.yml to try setting up builds with travis-ci.org
    
    Signed-off-by: Adam King <rak@linux.vnet.ibm.com>

---
+++ buildAll.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+################################################################################
+#                                                                              #
+#  Script to build and test all urbancode Docker images                        #
+#                                                                              #
+#                                                                              #
+#  Usage : buildAll.sh							                                           #
+#                                                                              #
+################################################################################
+
+while read -r imageName buildContextDirectory
+do
+  ./tools/build.sh $imageName $buildContextDirectory
+
+  if [ $? != 0 ]; then
+    echo "Failed at image $imageName - exiting"
+    exit 1
+  fi
+
+done < "images.txt"
